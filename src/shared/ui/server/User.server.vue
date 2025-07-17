0a0456ec5ad5d7b4aba8e2bcbb05c70e5001b85e
Artem Loshinin <109870624+artem32777@users.noreply.github.com>
2025-07-17 12:49:46 +0400

    first commit

---
+++ src/shared/ui/server/User.server.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = usePrismaClient().$extends(withAccelerate())
+const user = await prisma.user.findMany({
+	where: { name: 'vasya777' },
+})
+</script>
+
+<template>
+	<div>
+		{{ user ? user : 'No user has been added yet.' }}
+	</div>
+</template>
